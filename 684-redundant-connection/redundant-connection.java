84ad09d47e772cf8328cba5a54a6956f9876feda
Khushi Panwar <149927020+khushi7983@users.noreply.github.com>
2025-01-29 14:29:30 +0530

    Time: 6 ms (19.08%) | Memory: 44.9 MB (22.00%) - LeetSync

---
+++ 684-redundant-connection/redundant-connection.java
@@ -0,0 +1,37 @@
+class Solution {
+    public boolean dfs(Map<Integer, List<Integer>> adj, int u, int v, boolean[] visited) {
+        visited[u] = true;
+        if (u == v) {
+            return true;
+        }
+        for (int ngbr : adj.get(u)) {
+            if (visited[ngbr]) continue;
+            if (dfs(adj, ngbr, v, visited)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public int[] findRedundantConnection(int[][] edges) {
+        int n = edges.length;
+        Map<Integer, List<Integer>> adj = new HashMap<>();
+        
+        for (int[] edge : edges) {
+            int u = edge[0], v = edge[1];
+            
+            if (adj.containsKey(u) && adj.containsKey(v)) {
+                boolean[] visited = new boolean[n + 1];
+                if (dfs(adj, u, v, visited)) {
+                    return edge;
+                }
+            }
+            
+            adj.putIfAbsent(u, new ArrayList<>());
+            adj.putIfAbsent(v, new ArrayList<>());
+            adj.get(u).add(v);
+            adj.get(v).add(u);
+        }
+        return new int[0];
+    }
+}
