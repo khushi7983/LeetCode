fb804c98b235bf56203bafb05843c26a6ae39db5
Khushi Panwar <149927020+khushi7983@users.noreply.github.com>
2025-02-05 23:38:35 +0530

    Time: 0 ms (100.00%) | Memory: 41.6 MB (58.39%) - LeetSync

--- 1915-check-if-one-string-swap-can-make-strings-equal/check-if-one-string-swap-can-make-strings-equal.java
+++ 1915-check-if-one-string-swap-can-make-strings-equal/check-if-one-string-swap-can-make-strings-equal.java
@@ -3,31 +3,35 @@ class Solution {
         
         int n = s1.length();
 
-        if(s1==s2){
+          if (s1.equals(s2)) {
             return true;
         }
         int diffs = 0;
 
-        int freq1[] =  new int[26];
-        int freq2[] =  new int[26];  
+        int firstIdx =0;
+        int SecIdx = 0;        
 
         for(int i= 0 ; i <n ; i++){
             if(s1.charAt(i) != s2.charAt(i)){
                 diffs++;
-            }
+            
             if(diffs > 2){
                 return false;
   
             }
-            freq1[s1.charAt(i) - 'a']++;
-            freq2[s2.charAt(i) - 'a']++;
-        }
-             for(int i=0; i<26 ; i++){
-                if(freq1[i] != freq2[i])
-                return false;
-            }        
+            else if(diffs == 1){
+                firstIdx = i;
+            }
+            else{
+                SecIdx = i;
+            }
 
-        return true; 
+           }  
+
+        }   
+
+            return s1.charAt(firstIdx) == s2.charAt(SecIdx) && s1.charAt(SecIdx) == s2.charAt(firstIdx);
+
 
     }
 }
