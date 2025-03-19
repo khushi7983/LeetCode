323c297f7eef1c725b51fb7524747dc3120631a6
Khushi Panwar <149927020+khushi7983@users.noreply.github.com>
2025-03-19 18:48:59 +0530

    Time: 0 ms (100.00%) | Memory: 44.3 MB (71.44%) - LeetSync

--- 110-balanced-binary-tree/balanced-binary-tree.java
+++ 110-balanced-binary-tree/balanced-binary-tree.java
@@ -13,25 +13,63 @@
  *     }
  * }
  */
+// class Solution {
+//     public int levels( TreeNode root ){
+//         if(root == null) return 0;
+//         return 1+ Math.max(levels(root.left) , levels(root.right));
+      
+//     }
+//     public boolean isBalanced(TreeNode root) {
+       
+//        if(root == null) return true;
+
+//        int diff = Math.abs(levels(root.left) - levels(root.right));
+//        if(diff > 1){
+//         return false;
+//        }
+//        boolean lst = isBalanced(root.left);
+//        if(lst == false) return false;
+//        boolean rst = isBalanced(root.right);
+//        if(rst == false) return false;
+       
+//        return true;
+//     }
+// }
+
+
 class Solution {
-    public int levels( TreeNode root ){
+    public int levels( TreeNode root , boolean[] ans ){
         if(root == null) return 0;
-        return 1+ Math.max(levels(root.left) , levels(root.right));
+
+            int lst = levels(root.left , ans);
+            int rst =  levels(root.right , ans);
+
+            int diff  = Math.abs(lst - rst);
+            if(diff > 1){
+                ans[0] = false;
+            }
+
+        return 1+ Math.max(lst, rst);
       
     }
-    public boolean isBalanced(TreeNode root) {
-       
-       if(root == null) return true;
+    public boolean isBalanced(TreeNode root ) {
 
-       int diff = Math.abs(levels(root.left) - levels(root.right));
-       if(diff > 1){
-        return false;
-       }
-       boolean lst = isBalanced(root.left);
-       if(lst == false) return false;
-       boolean rst = isBalanced(root.right);
-       if(rst == false) return false;
-       
-       return true;
+       boolean[] ans = {true};
+       levels(root , ans);
+       return ans[0];
+      
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
