5a5bc95f6d1ed09a8a3c423aa38bee4397a3f277
Khushi Panwar <149927020+khushi7983@users.noreply.github.com>
2025-06-10 03:41:44 +0530

    Time: 5 ms (29.13%) | Memory: 48 MB (22.82%) - LeetSync

--- 64-minimum-path-sum/minimum-path-sum.java
+++ 64-minimum-path-sum/minimum-path-sum.java
@@ -3,20 +3,21 @@ class Solution {
        int m = grid.length;
        int n =  grid[0].length;
        int dp[][] =  new int[m][n];
-       for(int row[] : dp){
-        Arrays.fill(row,-1);
-       }
-       return f(m-1,n-1,grid,dp); 
-    }
-    public int f(int i, int j,int[][] grid, int[][] dp){
-        if(i==0 && j==0) return grid[0][0];
-        if(i <0 || j<0){
-            return (int) Math.pow(10, 9);
-        }
-        if(dp[i][j] != -1) return dp[i][j];
-        int up =  grid[i][j] + f(i-1,j,grid,dp);
-        int left = grid[i][j] + f(i,j-1,grid,dp);
+      
 
-       return dp[i][j] = Math.min(up,left);
+       
+       for(int i=0; i<m; i++){
+         for(int j=0; j<n; j++){
+          
+           if( i==0 && j==0) dp[i][j] = grid[0][0];
+           else{
+             int up = (i>0)?   dp[i-1][j] : (int) Math.pow(10,9);
+             int left = (j>0)?  dp[i][j-1] : (int) Math.pow(10,9);
+            dp[i][j] = grid[i][j]+ Math.min(up,left);
+           }
+
+         }
+       }
+       return dp[m-1][n-1];
     }
 }
