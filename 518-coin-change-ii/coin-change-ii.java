376e15c4102276634b6505ed0251913f442af4ba
Khushi Panwar <149927020+khushi7983@users.noreply.github.com>
2025-06-15 02:24:41 +0530

    Time: 8 ms (70.05%) | Memory: 49.6 MB (23.13%) - LeetSync

--- 518-coin-change-ii/coin-change-ii.java
+++ 518-coin-change-ii/coin-change-ii.java
@@ -2,22 +2,26 @@ class Solution {
     public int change(int amount, int[] coins) {
         int n = coins.length;
         int dp[][] = new int[n][amount + 1];
-         for(int rows[] : dp){
-            Arrays.fill(rows,-1);
+
+        // base case
+         for (int i = 0; i <= amount; i++) {
+            if (i % coins[0] == 0)
+                dp[0][i] = 1;
          }
-         return f(n-1,amount,coins,dp);
+
+         for(int i=1; i<n; i++){
+            for(int j=0; j<=amount; j++){
+                  int notTake = dp[i-1][j];
+                  int take = 0;
+                  if(j >= coins[i]){
+                  take = dp[i][j - coins[i]];
+                  }
+                dp[i][j] = take + notTake;
+                }
+            }
+           
+
+         return dp[n-1][amount];
     }
-    public int f(int i,int amount, int[] coins , int[][] dp ){
-        // Base Case
-        if(i == 0){
-            return (amount % coins[0] == 0) ? 1 : 0;
-        }
-        if(dp[i][amount] != -1) return dp[i][amount];
-        int notTake = f(i-1,amount,coins,dp);
-        int take = 0;
-        if(amount >= coins[i]){
-            take = f(i,amount-coins[i],coins,dp);
-        }
-        return dp[i][amount] = take + notTake;
-    }
+    
 }
