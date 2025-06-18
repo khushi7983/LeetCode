4652869596944b18dd3542ec92bd9199a05183e2
Khushi Panwar <149927020+khushi7983@users.noreply.github.com>
2025-06-19 01:43:10 +0530

    Time: 5 ms (61.70%) | Memory: 45.3 MB (18.39%) - LeetSync

--- 72-edit-distance/edit-distance.java
+++ 72-edit-distance/edit-distance.java
@@ -2,28 +2,32 @@ class Solution {
     public int minDistance(String word1, String word2) {
         int n = word1.length();
         int m = word2.length();
-        int dp[][] =  new int[n][m];
-        for(int row[] : dp){
-            Arrays.fill(row,-1);
+        int dp[][] =  new int[n+1][m+1];
+        
+        for(int i=0; i<=n; i++){
+            dp[i][0] = i;
         }
-        return f(n-1,m-1, word1,word2,dp);
-    }
-    public int f(int i, int j ,String s1, String s2, int[][] dp){
+        for(int j=0; j<=m; j++){
+            dp[0][j] = j;
+        }
+       
+        for(int i=1; i<=n; i++){
+            for(int j=1; j<=m; j++){
 
-         if(i < 0) return j+1;
-         if(j < 0) return i+1;
+             if(word1.charAt(i-1) == word2.charAt(j-1)){
+              dp[i][j] =  dp[i-1][j-1];
+             }
 
-         if(dp[i][j] != -1) return dp[i][j];
-
-         if(s1.charAt(i) == s2.charAt(j)){
-            return 0 + f(i-1,j-1,s1,s2,dp);
-         }
-
-
-         int insert =  1 + f(i,j-1,s1,s2,dp);
-         int delete =  1 + f(i-1,j,s1,s2,dp);
-         int replace =  1 + f(i-1,j-1,s1,s2,dp);
-
-        return dp[i][j] = Math.min(insert,Math.min(delete,replace));
+             else{
+             int insert =  1 +dp[i][j-1];
+             int delete =  1 + dp[i-1][j];
+             int replace =  1 + dp[i-1][j-1];
+              
+              dp[i][j] = Math.min(insert,Math.min(delete,replace));
+              
+             }
+            }
+        }
+        return dp[n][m];
     }
 }
