3251b6c1e8d55a6e58cf19272e2e793229518de1
Khushi Panwar <149927020+khushi7983@users.noreply.github.com>
2025-06-19 15:29:24 +0530

    Time: 24 ms (28.57%) | Memory: 45 MB (59.94%) - LeetSync

--- 44-wildcard-matching/wildcard-matching.java
+++ 44-wildcard-matching/wildcard-matching.java
@@ -2,36 +2,30 @@ class Solution {
     public boolean isMatch(String s, String p) {
         int n = s.length();
         int m = p.length();
-        int dp[][] = new int[n][m];
-        for(int row[] : dp){
-            Arrays.fill(row,-1);
+        boolean dp[][] = new boolean[n+1][m+1];
+
+          dp[0][0] = true;
+         
+        for (int j = 1; j <= m; j++) {
+            if (p.charAt(j - 1) == '*') {
+                dp[0][j] = dp[0][j - 1];
+            }
         }
-        return f(n-1,m-1,s,p,dp) == 1;
-    }
-    static boolean isAllStars(String p, int j) {
-    for (int i = 0; i <= j; i++) {
-      if (p.charAt(i) != '*')
-        return false;
-    }
-    return true;
-   }
-    public int f(int i, int j, String s, String p, int dp[][]){
-        if(i<0 && j<0) return 1;
-        if(j<0) return 0;
-        if(i<0) return isAllStars(p, j) ? 1 : 0; 
         
-        if(dp[i][j] != -1){
-            return dp[i][j];
+        for(int i=1; i<=n; i++){
+            for(int j=1; j<=m; j++){
+                if(s.charAt(i-1) == p.charAt(j-1) || p.charAt(j-1) == '?'){
+                dp[i][j] = dp[i-1][j-1];
+                }
+                else if (p.charAt(j-1) == '*') {
+                dp[i][j] = dp[i - 1][j]  || dp[i][j-1] ;
+                } else {
+                dp[i][j] = false;
+                }
+            }
         }
 
-        if(s.charAt(i) == p.charAt(j) || p.charAt(j) == '?'){
-            dp[i][j] = f(i-1,j-1,s,p,dp);
-        }
-        else if (p.charAt(j) == '*') {
-            dp[i][j] = (f(i - 1, j, s, p, dp) == 1 || f(i, j - 1, s, p, dp) == 1) ? 1 : 0;
-        } else {
-            dp[i][j] = 0;
-        }
-        return dp[i][j];
+
+        return dp[n][m];
     }
 }
