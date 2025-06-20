c232473fc286741ec118b9eb8fd57e9c78e62d04
Khushi Panwar <149927020+khushi7983@users.noreply.github.com>
2025-06-21 02:55:04 +0530

    Time: 60 ms (39.28%) | Memory: 63.9 MB (37.39%) - LeetSync

--- 123-best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.java
+++ 123-best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.java
@@ -1,35 +1,46 @@
 class Solution {
     public int maxProfit(int[] prices) {
         int n =  prices.length;
-        int dp[][][] =  new int[n][2][3];
-        for(int i=0; i<n; i++){
-            for(int j=0; j<2; j++){
-                Arrays.fill(dp[i][j], -1);
+         if (n <= 1) return 0;
+        int dp[][][] =  new int[n+1][2][3];
+        
+        for(int i = 0; i<n; i++){
+            for(int j=0; j<=1; j++){
+                dp[i][j][0] = 0;
             }
         }
-        return f(0,1,2,prices,dp);
-    }
-    public int f(int i, int buy,int cap,int[] prices, int[][][] dp){
-
-       
-        if(i == prices.length) return 0;
-        if(cap == 0) return 0;
-         
-        if(dp[i][buy][cap] != -1) return dp[i][buy][cap];
-
-        int profit = 0;
-        if(buy == 1){
-            int buyStock = -prices[i] + f(i+1,0,cap,prices,dp);
-            int notBuyStock = 0 + f(i+1,1,cap,prices,dp);
-
-             profit  = Math.max(buyStock,notBuyStock);
+        for(int j = 0; j<=1; j++){
+            for(int k=0; k<=2; k++){
+                dp[n][j][k] = 0;
+            }
         }
-        else{
-            int sell = prices[i] +f(i+1,1,cap-1,prices,dp);
-            int notSell = 0 + f(i+1,0,cap,prices,dp);
+        
 
-             profit = Math.max(sell,notSell); 
+        for(int i=n-1; i>=0; i--){
+            for(int j=0; j<=1; j++){
+                for(int k=1; k<=2; k++){
+                if(j==1){
+                int buyStock = -prices[i] + dp[i+1][0][k];
+                int notBuyStock =  dp[i+1][1][k];
+
+                dp[i][j][k] = Math.max(buyStock, notBuyStock); 
+                }
+                else{
+                  int sellStock = 0;
+                    if (k > 0)   
+                
+                     sellStock = prices[i] +dp[i+1][1][k-1];
+                 int notSellStock =  dp[i+1][0][k];
+
+                  dp[i][j][k]  = Math.max(sellStock,notSellStock);
+                }
+              }
+
+            }
         }
-        return dp[i][buy][cap] = profit;
+        
+
+        return dp[0][1][2];
     }
 }
+
