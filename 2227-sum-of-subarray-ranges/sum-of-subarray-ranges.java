254b6f53eab26e4eac11aea2817a25395c5100ee
Khushi Panwar <149927020+khushi7983@users.noreply.github.com>
2025-07-05 23:39:39 +0530

    Time: 23 ms (48.49%) | Memory: 45.2 MB (10.06%) - LeetSync

--- 2227-sum-of-subarray-ranges/sum-of-subarray-ranges.java
+++ 2227-sum-of-subarray-ranges/sum-of-subarray-ranges.java
@@ -1,20 +1,54 @@
 class Solution {
     public long subArrayRanges(int[] nums) {
         int n = nums.length;
+        int[] prevGreater = new int[n];
+        int[] nextGreater = new int[n];
+        int[] prevSmaller = new int[n];
+        int[] nextSmaller = new int[n];
 
-        long sum = 0;
-        for(int i=0;i<n;i++){
-            int max = Integer.MIN_VALUE, min = Integer.MAX_VALUE;
-            for(int j=i; j<n; j++){
-                max = Math.max(max,nums[j]);
-                min = Math.min(min,nums[j]);
-                
-                int range = max - min;
-
-                sum += range;
-
+        Stack<Integer> st = new Stack<>();
+        for (int i = 0; i < n; i++) {
+            while (!st.isEmpty() && nums[st.peek()] < nums[i]) {
+                st.pop();
             }
+            prevGreater[i] = st.isEmpty() ? -1 : st.peek();
+            st.push(i);
         }
-        return sum;
+
+        st.clear();
+        for (int i = n - 1; i >= 0; i--) {
+            while (!st.isEmpty() && nums[st.peek()] <= nums[i]) {
+                st.pop();
+            }
+            nextGreater[i] = st.isEmpty() ? n : st.peek();
+            st.push(i);
+        }
+
+        st.clear();
+        for (int i = 0; i < n; i++) {
+            while (!st.isEmpty() && nums[st.peek()] > nums[i]) {
+                st.pop();
+            }
+            prevSmaller[i] = st.isEmpty() ? -1 : st.peek();
+            st.push(i);
+        }
+
+        st.clear();
+        for (int i = n - 1; i >= 0; i--) {
+            while (!st.isEmpty() && nums[st.peek()] >= nums[i]) {
+                st.pop();
+            }
+            nextSmaller[i] = st.isEmpty() ? n : st.peek();
+            st.push(i);
+        }
+
+        long total = 0;
+        for (int i = 0; i < n; i++) {
+            long maxCount = (long)(i - prevGreater[i]) * (nextGreater[i] - i);
+            long minCount = (long)(i - prevSmaller[i]) * (nextSmaller[i] - i);
+            total += (maxCount - minCount) * nums[i];
+        }
+
+        return total;
     }
 }
