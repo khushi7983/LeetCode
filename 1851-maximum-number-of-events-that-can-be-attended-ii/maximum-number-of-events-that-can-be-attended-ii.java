15583d8f6a13370091016ab7963f1d974d1fe71b
Khushi Panwar <149927020+khushi7983@users.noreply.github.com>
2025-07-09 03:16:04 +0530

    Time: 194 ms (9.87%) | Memory: 121.3 MB (91.03%) - LeetSync

--- 1851-maximum-number-of-events-that-can-be-attended-ii/maximum-number-of-events-that-can-be-attended-ii.java
+++ 1851-maximum-number-of-events-that-can-be-attended-ii/maximum-number-of-events-that-can-be-attended-ii.java
@@ -3,32 +3,29 @@ import java.util.*;
 class Solution {
     public int maxValue(int[][] events, int k) {
         int n = events.length;
-        int dp[][] = new int[n][k+1];
-        for(int i=0; i<n; i++){
-            Arrays.fill(dp[i],-1);
-        }
+
         Arrays.sort(events, (a, b) -> a[0] - b[0]);
-        return f(0, k, events,dp);
 
+        
+        int[][] dp = new int[n + 1][k + 1]; 
+
+        for (int i = n - 1; i >= 0; i--) {
+            for (int j = 1; j <= k; j++) {
+
+               
+                int notAttend = dp[i + 1][j];
+
+                
+                int nextIndex = findNext(events, events[i][1]);
+                int attend = events[i][2] + dp[nextIndex][j - 1];
+
+                dp[i][j] = Math.max(attend, notAttend);
+            }
+        }
+
+        return dp[0][k];
     }
 
-
-    public int f(int i, int k, int[][] events, int dp[][]) {
-        int n = events.length;
-        if (k == 0 || i == n) return 0;
-         
-        if(dp[i][k] != -1) return dp[i][k];
-
-        int notAttend = f(i + 1, k, events ,dp);
-
-        // Attend 
-        int nextIndex = findNext(events, events[i][1]);
-        int attend = events[i][2] + f(nextIndex, k - 1, events ,dp);
-
-        return dp[i][k] = Math.max(attend, notAttend);
-    }
-
-    // Find the next index with startDay > current endDay using binary search
     private int findNext(int[][] events, int currEnd) {
         int low = 0, high = events.length;
         while (low < high) {
