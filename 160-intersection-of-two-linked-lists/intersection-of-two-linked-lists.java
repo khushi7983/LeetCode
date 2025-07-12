a64a21a8c47fb7fb19d84ba87d6437be475475d4
Khushi Panwar <149927020+khushi7983@users.noreply.github.com>
2025-07-13 03:20:04 +0530

    Time: 1 ms (99.90%) | Memory: 48.5 MB (68.30%) - LeetSync

--- 160-intersection-of-two-linked-lists/intersection-of-two-linked-lists.java
+++ 160-intersection-of-two-linked-lists/intersection-of-two-linked-lists.java
@@ -11,42 +11,42 @@
  */
 public class Solution {
     public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
-        int lenA = 0;
-        int lenB = 0;
+
         ListNode tempA = headA;
         ListNode tempB = headB;
-        
+
+        int lenA = 0;
         while( tempA != null){
             tempA = tempA.next;
             lenA++;
         }
+
+        int lenB = 0;
         while( tempB != null){
             tempB = tempB.next;
             lenB++;
         }
+
         tempA = headA;
         tempB = headB;
 
-        if(lenA > lenB){
-            for( int i=0; i<(lenA-lenB) ; i++){
-                tempA =tempA.next;
-            }
-           
-            
-        }
-        else{
-            for( int i=0; i<(lenB-lenA) ; i++){
-                tempB =tempB.next;
-            }
-            
-        }
-        while( tempA != tempB){
+        if( lenA > lenB){
+            for(int i=0; i<lenA - lenB; i++){
                 tempA = tempA.next;
-                tempB = tempB.next;
-
             }
+        }
+        else {
+            for(int i=0; i<lenB - lenA; i++){
+                tempB = tempB.next;
+            }
+        }
 
-            return tempA;
-     
+        while(tempA != tempB){
+            tempA = tempA.next;
+            tempB = tempB.next;
+        }
+        
+        return tempA;
+        
     }
 }
