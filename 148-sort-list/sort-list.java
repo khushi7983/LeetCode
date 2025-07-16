ef4ba2cb49270ae58671f010873cd3ef8846b725
Khushi Panwar <149927020+khushi7983@users.noreply.github.com>
2025-07-17 02:01:24 +0530

    Time: 9 ms (96.21%) | Memory: 57.2 MB (30.46%) - LeetSync

--- 148-sort-list/sort-list.java
+++ 148-sort-list/sort-list.java
@@ -9,50 +9,47 @@
  * }
  */
 class Solution {
-    public ListNode  merge(ListNode list1, ListNode list2) {
-        ListNode dummy = new ListNode(100);
-        ListNode temp = dummy;
-        ListNode temp1 = list1;
-        ListNode temp2 = list2;
-
-        while(temp1 !=null && temp2 != null){
-            if(temp1.val <= temp2.val){
-                temp.next = temp1;
-                temp1 = temp1.next;
-            }
-            else{
-                temp.next = temp2;
-                temp2 = temp2.next;
-            }
-            temp = temp.next;
-        }
-
-        if(temp1 == null){
-            temp.next = temp2;
-        }
-        else {
-            temp.next = temp1;
-        }
-        return dummy.next;
-    }
     public ListNode sortList(ListNode head) {
-        if(head == null || head.next == null){
-            return head;
+        if(head == null || head.next == null) return head;
+        ListNode a = head;
+        ListNode slow = head;
+        ListNode fast = head;
+        while(fast.next != null && fast.next.next != null){
+            slow = slow.next;
+            fast = fast.next.next;
         }
-            ListNode a = head;
-            ListNode slow = head;
-            ListNode fast = head;
-            while( fast.next!= null && fast.next.next != null ){
-                slow = slow.next;
-                fast = fast.next.next;  
-            } 
-            ListNode b = slow.next;
-            slow.next = null; 
 
-           a =  sortList(a); 
-           b =  sortList(b); 
+        ListNode b = slow.next;
+        slow.next = null;
+        a = sortList(a);
+        b = sortList(b);
 
-           ListNode ans = merge( a , b);
-           return ans;
+        return merge(a,b);
     }
+     public ListNode merge(ListNode list1, ListNode list2) {
+        
+        ListNode dummy = new ListNode(0);
+        ListNode tail = dummy;
+
+        while (list1 != null && list2 != null) {
+            if (list1.val < list2.val) {
+                tail.next = list1;
+                list1 = list1.next;
+            } else {
+                tail.next = list2;
+                list2 = list2.next;
+            }
+            tail = tail.next;
+        }
+
+        
+        if (list1 != null) {
+            tail.next = list1;
+        } else {
+            tail.next = list2;
+        }
+
+        return dummy.next;  
+    }
+
 }
