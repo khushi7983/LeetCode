3b9756284c0bb3b08189acb4f2ec225a4ed26f5e
Khushi Panwar <149927020+khushi7983@users.noreply.github.com>
2025-07-27 02:40:07 +0530

    Time: 0 ms (100.00%) | Memory: 42 MB (89.50%) - LeetSync

--- 48-rotate-image/rotate-image.java
+++ 48-rotate-image/rotate-image.java
@@ -1,24 +1,34 @@
 class Solution {
-    public void rotate(int[][] arr) {
-        int n = arr.length;
-        for( int i=0 ; i<n ; i++){
-           for(int j = i+1 ; j<n; j++){
-             int temp =  arr[i][j] ;
-             arr[i][j]= arr[j][i];
-             arr[j][i] = temp;
-            }
-        }    
+    public void rotate(int[][] matrix) {
+        
+        int n = matrix.length;
 
-        for(int i =0 ; i<n ; i++){
-            int left = 0;
-            int right = n-1;
-            while(left<right){
-                int temp =  arr[i][left] ;
-                 arr[i][left]= arr[i][right];
-                 arr[i][right] = temp;
-                 left++;
-                 right--;
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
             }
         }
+
+        for (int i = 0; i < n; i++) {
+            reverseRow(matrix[i]);
+        }
+
+       
+        
     }
+        
+    private void reverseRow(int[] row) {
+    int start = 0;
+    int end = row.length - 1;
+         while (start < end) {
+            int temp = row[start];
+            row[start] = row[end];
+            row[end] = temp;
+            start++;
+            end--;
+    
+        }
+    }    
 }
