6b61f1cabf4d4a3f8f52e37d3d7ff9217f5aa7fe
Khushi Panwar <149927020+khushi7983@users.noreply.github.com>
2025-07-30 01:26:59 +0530

    Time: 17 ms (73.43%) | Memory: 44.3 MB (29.35%) - LeetSync

--- 18-4sum/4sum.java
+++ 18-4sum/4sum.java
@@ -3,27 +3,34 @@ import java.util.*;
 class Solution {
     public List<List<Integer>> fourSum(int[] nums, int target) {
         int n = nums.length;
-        Set<List<Integer>> ans = new HashSet<>();
+        List<List<Integer>> ans = new ArrayList<>();
         
         Arrays.sort(nums); 
 
         for (int i = 0; i < n; i++) {
+            if( i > 0 && nums[i] == nums[i-1]) continue;
             for (int j = i + 1; j < n; j++) {
-                HashSet<Long> set = new HashSet<>();
-                for (int k = j + 1; k < n; k++) {
-                    long sum = 1L * nums[i] + nums[j] + nums[k]; // Use long to avoid overflow
-                    long fourth = target - sum;
-                    
-                    if (set.contains(fourth)) {
-                        List<Integer> temp = Arrays.asList(nums[i], nums[j], nums[k], (int) fourth);
-                        Collections.sort(temp); // Sort to handle different orders of same values
-                        ans.add(temp);
+                 if( j != i + 1 && nums[j] == nums[j-1]) continue;
+                 int k = j + 1;
+                 int l = n - 1;
+                 while(k < l ){
+                    long sum = nums[i];
+                      sum += nums[j];
+                      sum += nums[k];
+                      sum += nums[l];
+                    if(sum == target){ List<Integer> list = Arrays.asList(nums[i],nums[j],nums[k],nums[l]);
+                    k++; l--;
+                     while (k < l && nums[k] == nums[k - 1]) k++;
+                     while (k < l && nums[l] == nums[l + 1]) l--;
+                    ans.add(list);
                     }
-                    set.add((long) nums[k]);
-                }
+                    else if(sum > target) l--;
+                    else k++;
+                 }
+                
             }
         }
 
-        return new ArrayList<>(ans);
+        return ans;
     }
 }
