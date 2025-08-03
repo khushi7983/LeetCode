ace0a89af4f5633cfd613cf03430b11ca74c6385
Khushi Panwar <149927020+khushi7983@users.noreply.github.com>
2025-08-04 03:01:29 +0530

    Time: 0 ms (100.00%) | Memory: 43.3 MB (66.84%) - LeetSync

--- 81-search-in-rotated-sorted-array-ii/search-in-rotated-sorted-array-ii.java
+++ 81-search-in-rotated-sorted-array-ii/search-in-rotated-sorted-array-ii.java
@@ -1,35 +1,34 @@
 class Solution {
-    public boolean search(int[] arr, int target) {
-        int n = arr.length;
-        int low = 0;
-        int high = n-1;
-        while(low<=high){
-            int mid = (low + high)/2;
-            if(arr[mid]== target){
-                return true;
+    public boolean search(int[] nums, int target) {
+        int n = nums.length;
+       int l = 0;
+       int r = n-1; 
+
+       while(l <= r){
+        int mid = l + (r - l) / 2;
+        if(target == nums[mid]) return true;
+        if(nums[l] == nums[mid] && nums[mid] == nums[r]){
+            l++;
+            r--;
+            continue;
+        }
+        if(nums[mid] <= nums[r]){
+            if(target >= nums[mid] && target <= nums[r] ){
+                 
+                  l = mid + 1; 
             }
-            if(arr[low]==arr[mid]&& arr[mid]==arr[high]){
-                low = low + 1;
-                high = high - 1;
-             }
-            // left sorted 
-             else if(arr[low]<=arr[mid]){
-                 if(arr[low]<=target && target <=arr[mid]){
-                    high = mid - 1;
-                 }
-                 else{
-                    low = mid + 1;
-                }
+            else  r = mid - 1;
+
+        }
+        else{
+            if(target >= nums[l] && target <= nums[mid]){
+                r = mid - 1;
             }
             else{
-                if(arr[mid]<= target && target <= arr[high]){
-                    low = mid+1 ;
-                }
-                else{
-                    high = mid - 1;
-                }
+                l = mid + 1;
             }
         }
-        return false;
+       }
+       return false;
     }
 }
