764f6bb191fc8d1083203766ae628108588d7bea
Khushi Panwar <149927020+khushi7983@users.noreply.github.com>
2025-08-06 02:14:19 +0530

    Time: 0 ms (100.00%) | Memory: 50.2 MB (65.89%) - LeetSync

--- 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.java
+++ 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.java
@@ -1,33 +1,27 @@
 class Solution {
-    public int singleNonDuplicate(int[] arr) {
-        int n = arr.length;
-        int low = 1;
-        int high =  n-2 ;
-        
-        if(n == 1){
-            return arr[0];
-        }
-        if(arr[0]!= arr[1]){
-            return arr[0];
-        }
-        if(arr[n-2]!=arr[n-1]){
-            return arr[n-1];
-        }
-        while(low<=high){
+    public int singleNonDuplicate(int[] nums) {
+        int n = nums.length;
+        int l = 0; int r = n-1;
 
-            int mid = (low + high)/2 ;
+        if(n == 1) return nums[0];
+        if(nums[0] != nums[1]) return nums[0];
+        if(nums[n-1] != nums[n-2]) return nums[n-1];
 
-            if(arr[mid]!= arr[mid+1] && arr[mid-1]!= arr[mid]){
-                return arr[mid];
+        while(l <= r){
+           int mid = l + (r - l) / 2;
+
+            if(nums[mid] != nums[mid + 1] && nums[mid] != nums[mid - 1]) return nums[mid];
+
+            if(mid % 2 == 1 && nums[mid-1] == nums[mid] || mid % 2 == 0 && nums[mid] == nums[mid + 1]){
+              l = mid + 1;
+            } 
+            else{
+              r = mid - 1; 
             }
 
-            if((mid%2 == 1 && arr[mid-1] == arr[mid]) || (mid%2 == 0 && arr[mid] == arr[mid + 1])){
-                  low = mid + 1 ;
-            }
-            else {
-                high = mid - 1 ;
-            }
         }
+
         return -1;
+        
     }
 }
