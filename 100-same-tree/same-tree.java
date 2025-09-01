613bec214f0e77ab5552777b898cffab20fb9c8b
Khushi Panwar <149927020+khushi7983@users.noreply.github.com>
2025-09-02 03:04:02 +0530

    Time: 0 ms (100.00%) | Memory: 41.1 MB (70.03%) - LeetSync

--- 100-same-tree/same-tree.java
+++ 100-same-tree/same-tree.java
@@ -7,7 +7,7 @@
  *     TreeNode() {}
  *     TreeNode(int val) { this.val = val; }
  *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
+        this.val = val;
  *         this.left = left;
  *         this.right = right;
  *     }
@@ -15,14 +15,12 @@
  */
 class Solution {
     public boolean isSameTree(TreeNode p, TreeNode q) {
+        if(p == null && q == null) return true;
         
-        if(p == null && q == null){
-            return true;
-        }
-        if( p == null || q == null || p.val != q.val){
-            return false;
-        }
-        
-        return isSameTree(p.left , q.left)&&isSameTree(p.right , q.right);
+        if(p == null || q == null) return false;
+
+        if(p.val != q.val) return false;
+
+        return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
     }
 }
