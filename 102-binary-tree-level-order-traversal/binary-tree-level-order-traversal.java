715149fc98aa1c262c3e30147ffe63a927b8a665
Khushi Panwar <149927020+khushi7983@users.noreply.github.com>
2025-09-03 23:56:57 +0530

    Time: 108 ms (3.00%) | Memory: 45.8 MB (6.55%) - LeetSync

--- 102-binary-tree-level-order-traversal/binary-tree-level-order-traversal.java
+++ 102-binary-tree-level-order-traversal/binary-tree-level-order-traversal.java
@@ -1,47 +1,30 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */                                  
- 
 class Solution {
+    public static int height(TreeNode root){
+        if(root == null) return -1  ;
+        if(root.left == null && root.right == null) return 0 ;
+        return 1 + Math.max(height(root.left) , height(root.right));
+    }
+    public static List<Integer>  nthlevel(TreeNode root , int n ){
+        List<Integer> result = new ArrayList<>();
+
+        if(root == null) return result  ;
+        if(n==1 ) result.add(root.val);
+        else {
+            result.addAll(nthlevel(root.left, n - 1));
+            result.addAll(nthlevel(root.right, n - 1));
+        }
+        return result ;
+
+    }
     public List<List<Integer>> levelOrder(TreeNode root) {
+       List<List<Integer>> levels = new ArrayList<>();
+        int h = height(root) + 1; // number of levels
 
-        List<List<Integer>> ans = new ArrayList<>();
-
-        if(root == null) return ans;
-
-        Queue<TreeNode> q = new LinkedList<TreeNode>();
-        q.offer(root);    
-
-        while(!q.isEmpty()){
-            int size = q.size();  // number of nodes in current level
-            List<Integer> level = new ArrayList<>();
-
-            for(int i = 0; i < size; i++){
-                TreeNode node = q.poll();
-                level.add(node.val);
-
-                if (node.left != null) q.offer(node.left);
-                if (node.right != null) q.offer(node.right);
-
-            }
-            ans.add(level);
+        for (int i = 1; i <= h; i++) {
+            levels.add(nthlevel(root, i));
         }
 
-        return ans;
-
-
+        return levels;
         
     }
 }
