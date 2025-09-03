8113fd0e99b9ec68bfab69180fb5a46b0b8ca445
Khushi Panwar <149927020+khushi7983@users.noreply.github.com>
2025-09-03 18:09:12 +0530

    Time: 0 ms (100.00%) | Memory: 41 MB (10.20%) - LeetSync

--- 62-unique-paths/unique-paths.java
+++ 62-unique-paths/unique-paths.java
@@ -1,22 +1,25 @@
 class Solution {
     public int uniquePaths(int m, int n) {
-        int dp[][] =  new int[m][n];
 
-       
-        
-        for(int i=0; i<m; i++){
-            for(int j=0; j<n; j++){
-                if( i == 0 && j==0)  dp[0][0] = 1;  
-                else{ 
-                    int up = (i > 0) ? dp[i - 1][j] : 0;
-                    int left = (j > 0) ? dp[i][j - 1] : 0;
-                    dp[i][j] = left+up;
-                }
-            
-            }
+        int dp[][] = new int[m][n];
+        for(int rows[] : dp){
+            Arrays.fill(rows,-1);
         }
-        return dp[m-1][n-1];
-
+        
+        return  f(m-1 , n-1, dp); 
     }
-    
+    public int f(int i , int j , int[][] dp){
+
+        if(i == 0 && j == 0) return 1;
+
+        if(i < 0 || j < 0) return 0;
+
+        if(dp[i][j] != -1) return dp[i][j];
+
+        int up = f(i - 1, j, dp);
+        int left = f(i , j - 1, dp);
+
+        return dp[i][j] = up + left;
+          
+    }
 }
