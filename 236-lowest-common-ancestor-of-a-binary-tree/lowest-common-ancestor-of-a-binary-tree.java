1cccdd8ee806481e43bb5d9a4bb5b105ec39cc19
Khushi Panwar <149927020+khushi7983@users.noreply.github.com>
2025-09-18 22:20:00 +0530

    Time: 6 ms (100.00%) | Memory: 44.6 MB (85.73%) - LeetSync

--- 236-lowest-common-ancestor-of-a-binary-tree/lowest-common-ancestor-of-a-binary-tree.java
+++ 236-lowest-common-ancestor-of-a-binary-tree/lowest-common-ancestor-of-a-binary-tree.java
@@ -7,45 +7,21 @@
  *     TreeNode(int x) { val = x; }
  * }
  */
-
 class Solution {
-    HashMap<TreeNode,TreeNode> parentMap = new HashMap<>();
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-        dfs(root, null, p, q);
-        
-        // Collect ancestors of p
-        Set<TreeNode> ancestors = new HashSet<>();
-        while (p != null) {
-            ancestors.add(p);
-            p = parentMap.get(p);
-        }
-        
-        while (q != null) {
-            if (ancestors.contains(q)) {
-                return q;  // LCA found
-            }
-            q = parentMap.get(q);
-        }
+        if(root == null) return null;
+        if(root == p || root == q) return root;
 
-        return null; // should not happen if both p and q are in tree
-            
-        
-    }
-    public void dfs(TreeNode node, TreeNode parent, TreeNode p, TreeNode q){
-        if(node == null) return;
+        TreeNode left =  lowestCommonAncestor(root.left,p,q);
+        TreeNode right = lowestCommonAncestor(root.right,p,q);
 
-        if(parent != null){
-            parentMap.put(node,parent);
+        if(left != null && right != null) return root;
+        if(left != null){
+            return left;
         }
-        // if both found 
-        if (parentMap.containsKey(p) && parentMap.containsKey(q)) {
-            return;
-        }
-         
-        dfs(node.left,node,p,q);
-        dfs(node.right,node,p,q);
+        else return right;
+
 
         
     }
-
 }
