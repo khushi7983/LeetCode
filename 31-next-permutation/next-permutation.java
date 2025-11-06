c417aa848fb585960ec366ff645695b3989d3409
Khushi Panwar <149927020+khushi7983@users.noreply.github.com>
2025-11-07 01:55:08 +0530

    Time: 0 ms (100.00%) | Memory: 44.5 MB (6.36%) - LeetSync

---
+++ 31-next-permutation/next-permutation.java
@@ -0,0 +1,33 @@
+class Solution {
+    public void nextPermutation(int[] nums) {
+        int n = nums.length;
+        int i = n - 2;
+        // Step 1: Find the first index 'i' from the end where nums[i] < nums[i+1]
+        while (i >= 0 && nums[i] >= nums[i + 1]) {
+            i--;
+        }
+        // Step 2: If such index is found, find the first index 'j' from the end where nums[j] > nums[i]
+        if (i >= 0) {
+            int j = n - 1;
+            while (nums[j] <= nums[i]) {
+                j--;
+            }
+            // Step 3: Swap nums[i] and nums[j]
+            swap(nums, i, j);
+        }
+        // Step 4: Reverse the part after index 'i'
+        reverse(nums, i + 1, n - 1);
+    }
+    private void swap(int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+    private void reverse(int[] nums, int start, int end) {
+        while (start < end) {
+            swap(nums, start, end);
+            start++;
+            end--;
+        }
+    }
+}
