0f36ca3cfc56a70d454a31008e6b1fb262e4ff3e
Khushi Panwar <149927020+khushi7983@users.noreply.github.com>
2025-11-22 01:45:05 +0530

    Time: 214 ms (27.08%) | Memory: 46.9 MB (29.85%) - LeetSync

--- 2059-unique-length-3-palindromic-subsequences/unique-length-3-palindromic-subsequences.java
+++ 2059-unique-length-3-palindromic-subsequences/unique-length-3-palindromic-subsequences.java
@@ -1,33 +1,36 @@
 class Solution {
     public int countPalindromicSubsequence(String s) {
-
-
-        HashSet<Character> set = new HashSet<>();
         int n = s.length();
-        for(int i=0;i<n;i++){
-            set.add(s.charAt(i)); 
+        HashSet<Character> letters = new HashSet<>();
+
+        for(int i=0; i<n; i++){
+            letters.add(s.charAt(i));
+        }
+        int result  = 0;
+
+        for(int letter : letters){
+            int firstIdx  = -1;
+            int lastIdx = -1;
+
+            for(int i = 0; i<n; i++){
+                if(s.charAt(i) == letter){
+                    if(firstIdx == -1){
+                        firstIdx = i;
+                    }
+                    else{
+                        lastIdx = i;
+                    }
+                } 
+            }
+            HashSet<Character> st = new HashSet<>();
+            for(int middle = firstIdx+1; middle <= lastIdx -1; middle++){
+                st.add(s.charAt(middle)); 
+            }
+            
+            result += st.size();
         }
 
-        int count=0;
-        for(char ch : set){
-            int first=-1;
-            int last = -1;
-            for(int i=0;i<n;i++){
-                if(ch == s.charAt(i)){
-                    if(first == -1){
-                        first = i;
-                    }
-                    last = i;
-                }
-            }
-            if(first == last) continue;
-            HashSet<Character> set1 = new HashSet<>();
-            for(int i=first+1;i<last;i++){
-                set1.add(s.charAt(i));
-            }
-            count += set1.size();
-        }
-        return count;
+        return result;
+        
     }
 }
-        
